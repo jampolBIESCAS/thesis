26c23ba8b895cb156950693e07ed696e5d404430
jampolBIESCAS <jepoybiescas.16@gmail.com>
2022-04-28 17:21:18 +0800

    update settings etc

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ics.wsgi --log-file -
--- thesis/settings.py
+++ thesis/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(5z&y3iog^42g9b)x!_(17q&d1ta^jmx-uk_y(h4efmvqbl71x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bscsthesis.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
