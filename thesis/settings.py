a15471eb50be2e3e4e6a3ba1e04b9dfa47d2570f
jampolBIESCAS <jepoybiescas.16@gmail.com>
2022-04-29 00:38:25 +0800

    git commit

--- thesis/settings.py
+++ thesis/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(5z&y3iog^42g9b)x!_(17q&d1ta^jmx-uk_y(h4efmvqbl71x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['bscsthesis.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['thesisrp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
